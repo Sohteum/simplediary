a32bbe85c7a5af36eb65dc7b30342fc0685334ee
Sohteum <thtwk1123@gmail.com>
2023-09-06 12:19:28 +0900

    .

--- src/App.js
+++ src/App.js
@@ -3,6 +3,38 @@ import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
+const dummyList = [
+  {
+    id: 1,
+    author: "sohteum",
+    content: "하이1",
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 2,
+    author: "saddy",
+    content: "하이2",
+    emotion: 3,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 3,
+    author: "smilly",
+    content: "하이3",
+    emotion: 2,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 4,
+    author: "funnny",
+    content: "하이4",
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+
+]
+
 function App() {
 
 
@@ -10,7 +42,7 @@ function App() {
     <div className="App">
 
       <DiaryEditor />
-      <DiaryList />
+      <DiaryList diaryList={dummyList}/>
 
     </div>
   );
--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import DiaryItem from './DiaryItem';
 
-const DiaryList = () => {
+const DiaryList = ({ diaryList }) => {
+
     return (
         <div className='DiaryList'>
             <h2>일기리스트</h2>
+            <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+            <div>
+                {diaryList.map((it) => (
+                    <DiaryItem key={it.id} {...it}/>
+                    
+                ))}
+            </div>
         </div>
     );
 };
+
+DiaryList.defaultProps = {
+    diaryList: [],
+};//언디파인으로 오는 프롭스들에게 디폴드값을 줌
 
 export default DiaryList;
