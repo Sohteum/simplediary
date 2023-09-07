77045c47a11c88f0242fc370adcad0a1d6d9c8bb
Sohteum <thtwk1123@gmail.com>
2023-09-07 12:02:26 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,48 +1,64 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-const dummyList = [
-  {
-    id: 1,
-    author: "sohteum",
-    content: "하이1",
-    emotion: 5,
-    created_date: new Date().getTime()
-  },
-  {
-    id: 2,
-    author: "saddy",
-    content: "하이2",
-    emotion: 3,
-    created_date: new Date().getTime()
-  },
-  {
-    id: 3,
-    author: "smilly",
-    content: "하이3",
-    emotion: 2,
-    created_date: new Date().getTime()
-  },
-  {
-    id: 4,
-    author: "funnny",
-    content: "하이4",
-    emotion: 5,
-    created_date: new Date().getTime()
-  },
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "sohteum",
+//     content: "하이1",
+//     emotion: 5,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id: 2,
+//     author: "saddy",
+//     content: "하이2",
+//     emotion: 3,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id: 3,
+//     author: "smilly",
+//     content: "하이3",
+//     emotion: 2,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id: 4,
+//     author: "funnny",
+//     content: "하이4",
+//     emotion: 5,
+//     created_date: new Date().getTime()
+//   },
 
-]
+// ]
 
 function App() {
 
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0) //변수처럼 사용하는...?
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current
+    }
+    dataId.current += 1;
+    setData([newItem, ...data ])
+  }
 
   return (
     <div className="App">
 
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList}/>
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
 
     </div>
   );
